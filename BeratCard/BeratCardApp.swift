a6e8d8cbe0fd41a027b9a220856e17af301a356d
karayilanberat <karayilanberat@gmail.com>
2024-07-19 22:45:42 +0300

    Initial Commit

---
+++ BeratCard/BeratCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeratCardApp.swift
+//  BeratCard
+//
+//  Created by berat on 19.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct BeratCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
