85cc54efd42b83b5ac2302c21732dfe7f80c47b3
karayilanberat <karayilanberat@gmail.com>
2024-07-20 00:23:01 +0300

    End Of The Project

---
+++ BeratCard/CardView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BeratCard
+//
+//  Created by berat on 19.07.2024.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    var body: some View {
+        ZStack {
+            Color(.gray)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+#Preview {
+    CardView()
+}
---
+++ BeratCard/InfoView.swift
@@ -0,0 +1,18 @@
+//
+//  InfoView.swift
+//  BeratCard
+//
+//  Created by berat on 20.07.2024.
+//
+
+import SwiftUI
+
+struct InfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfoView()
+}
